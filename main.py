3fc6d69ab2dccb8476057451b48de931ed8a7791
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-13 23:14:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
-IMG_SIZE = 227
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positve'}
 
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
+st.title('Sentiment analysis from Vietnamese students’ feedback')
 
-st.title('Pneumonia prediction based on chest X-Ray image')
-
-input = open('lrc_xray.pkl', 'rb')
+input = open('lrc_vsfc.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Upload chest X-Ray image')
-uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
+st.header('Write a feedback')
+#################
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Test image')
 
     if st.button('Predict'):
-        image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
         feature_vector = np.array(image)
         label = str((model.predict(feature_vector))[0])
 
