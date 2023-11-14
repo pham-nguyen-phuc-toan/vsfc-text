083bd8bf29767a491adff4ef622aaf653df1ff3a
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 08:36:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positve'}
 
 st.title('Sentiment analysis from Vietnamese students’ feedback')
 
-input = open('lrc_vsfc.pkl', 'rb')
-model = pkl.load(input)
+input_ec = open('ec_vsfc.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vsfc.pkl', 'rb')
+model = pkl.load(input_md)
 
 st.header('Write a feedback')
 txt = st.text_area('', '')
 
 if txt != '':
     if st.button('Predict'):
-        feature_vector = txt
+        feature_vector = encoder.transform(txt)
         label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
