27bc0459cf871a5417e2bde0ce7f2db6d7764875
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 09:33:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positve'}
 
 st.title('Sentiment analysis from Vietnamese students’ feedback')
 
-image = Image.open('iris_flower.jpg')
+image = Image.open('feedback.jpg')
 st.image(image)
 
 input_ec = open('ec_vsfc.pkl', 'rb')
